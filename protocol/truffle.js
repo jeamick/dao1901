3e6ba87c3a139701c5e6364e66cc40c3774db579
Janaka-Steph <rochestephane@yahoo.fr>
2017-07-13 12:04:17 +0200

    truffle-lightwallet-provider

--- protocol/truffle.js
+++ protocol/truffle.js
@@ -1,17 +1,53 @@
+require('dotenv').config()
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
 require("babel-polyfill")
-
+const LightWalletProvider = require('@digix/truffle-lightwallet-provider')
+const { KEYSTORE, PASSWORD } = process.env
+if (!KEYSTORE || !PASSWORD) { throw new Error('You must export KEYSTORE and PASSWORD (see truffle.js)') }
 
 module.exports = {
   networks: {
     development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*',        // Match any network id
-      before_timeout: 200000, // specify Mocha timeout per network
-      test_timeout: 300000    // specify Mocha timeout per network
-    }
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'http://localhost:8545/',
+        debug: true, // optional, show JSON-RPC logs
+        prefund: 1e18, // optional, fund all lightwallet addresses (via coinbase) with this  of wei
+        pollingInterval: 500 // optional, polling interval for the provider (reduce for faster deploy with testRPC or kovan)
+      }),
+      network_id: '*',
+      //before_timeout: 200000, // specify Mocha timeout per network
+      //test_timeout: 300000    // specify Mocha timeout per network
+    },
+    kovan: {
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'https://kovan.infura.io/',
+        pollingInterval: 2000 // optional, polling interval for the provider (reduce for faster deploy with testRPC or kovan)
+      }),
+      gas: 4600000,
+      network_id: '42',
+    },
+    ropsten: {
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'https://ropsten.infura.io/',
+        pollingInterval: 4000 // optional, polling interval for the provider (reduce for faster deploy with testRPC or kovan)
+      }),
+      network_id: '3',
+    },
+    mainnet: {
+      provider: new LightWalletProvider({
+        keystore: "",
+        password: "",
+        rpcUrl: 'https://mainnet.infura.io/'
+      }),
+      network_id: '1',
+    },
   },
   mocha: {
     timeout: 200000,
